6403f275ad0d97e68f56311f4f41d7422b41abda
Rakhman Medzhidov <rakhman.medzhidov@axxonsoft.dev>
2025-03-01 02:40:21 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,47 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
+use std::fs;
 
-fn type_buildin(name: &str) -> String {
-    if name.starts_with("echo") || name.starts_with("exit") || name.starts_with("type") {
-        format!("{} is a shell builtin", name.trim())
-    } else {
-        format!("{}: not found", name.trim())
+fn find_exec_in_fs(path: &str, name: &str) -> io::Result<String> {
+    for p in path.split(":") {
+        let entries = fs::read_dir(p)?;
+
+        for entry in entries {
+            let entry = entry?;
+            let filename = entry.file_name();
+            if filename == name {
+                return Ok(p.to_string());
+            }
+        }
+    }
+
+    Err(io::Error::new(io::ErrorKind::NotFound, "File not found"))
+}
+
+fn find_exec_in_path(name: &str) -> Option<String> {
+    match env::var("PATH") {
+        Ok(path) => {
+            match find_exec_in_fs(path.as_str(), name) {
+                Ok(path) => Some(path),
+                Err(_) => None,
+            }
+        }
+        Err(err) => None
     }
 }
 
+fn type_buildin(name: &str) -> String {
+    if name.starts_with("echo") || name.starts_with("exit") || name.starts_with("type") {
+        return format!("{} is a shell builtin", name);
+    }
+    if let Some(path) = find_exec_in_path(name) {
+        return format!("{} is {}/{}", name, path, name);
+    }
+    format!("{} not found", name)
+}
+
 fn main() {
-    // Uncomment this block to pass the first stage
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -24,7 +55,7 @@ fn main() {
         } else if input.starts_with("echo ") {
             println!("{}", &input[5..].trim());
         } else if input.starts_with("type ") {
-            let msg = type_buildin(&input[5..]);
+            let msg = type_buildin(&input[5..].trim());
             println!("{}", msg);
         } else {
             println!("{}: command not found", input.trim());
