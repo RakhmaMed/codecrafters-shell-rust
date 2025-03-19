a6df20b6b66631b654ed1e34ecc5a4bcb46b647e
Rakhman Medzhidov <rakhman.medzhidov@axxonsoft.dev>
2025-03-20 02:14:47 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
 use std::env;
 use std::fs;
+#[allow(unused_imports)]
+use std::io::{self, Write};
 
 fn find_exec_in_fs(path: &str, name: &str) -> io::Result<String> {
     for p in path.split(":") {
@@ -20,27 +20,21 @@ fn find_exec_in_fs(path: &str, name: &str) -> io::Result<String> {
 }
 
 fn find_exec_in_path(name: &str) -> Option<String> {
-    match env::var("PATH") {
-        Ok(path) => {
-            match find_exec_in_fs(path.as_str(), name) {
-                Ok(path) => Some(path),
-                Err(_) => None,
-            }
-        }
-        Err(err) => None
-    }
+    let path = env::var("PATH").ok()?;
+    find_exec_in_fs(path.as_str(), name).ok()
 }
 
 fn type_buildin(name: &str) -> String {
-    let command = name.split_whitespace();
-    match command {
-        Some("echo") | Some("exit") | Some("type") => return format!("{} is a shell builtin", name)
+    if let Some(first) = name.split_whitespace().next() {
+        if ["echo", "exit", "type"].contains(&first) {
+            return format!("{} is a shell buildin", name);
+        }
     }
-    
+
     if let Some(path) = find_exec_in_path(name) {
         return format!("{} is {}/{}", name, path, name);
     }
-    
+
     format!("{} not found", name)
 }
 
@@ -56,10 +50,11 @@ fn main() {
         match input.trim() {
             "exit 0" => break,
             input if input.starts_with("echo ") => println!("{}", &input[5..]),
-            input if input.starts_with("type ") {
+            input if input.starts_with("type ") => {
                 let msg = type_buildin(&input[5..]);
                 println!("{}", msg);
-            },
-            _ => println!("{}: command not found", input.trim())
+            }
+            _ => println!("{}: command not found", input.trim()),
         }
+    }
 }
